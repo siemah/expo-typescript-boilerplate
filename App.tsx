02a48eff5b9b52414af94815e79cdf02be03e9e9
siemah <m_hamiche@inptic.edu.dz>
2021-07-17 09:28:08 +0100

    feat:load font before rendering the app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import * as Font from 'expo-font';
+
 import MainStack from './src/navigations/main';
+import { FontDisplay } from './src/constants/font';
+import { globalReducer } from './src/utils/store/reducers';
 
-
-
+const initialState = {
+  errors: {},
+  loading: true,
+  global: null,
+  data: {},
+}
 export default function App() {
+  let _isMounted = true;
+  const [state, dispatch] = useReducer(globalReducer, initialState);
+
+  useEffect(() => {
+    const loadAssetsAndInitialData = async () => {
+      try {
+        await Font.loadAsync({
+          [FontDisplay.PoppinsBold]: require('./assets/fonts/Poppins-Bold.ttf'),
+          [FontDisplay.PoppinsRegular]: require('./assets/fonts/Poppins-Regular.ttf'),
+        });
+        _isMounted && dispatch({
+          type: 'SET_LOADING',
+          payload: false
+        });
+      } catch (error) {
+        _isMounted && dispatch({
+          type: 'SET_LOADING',
+          payload: false
+        });
+      }
+    }
+    loadAssetsAndInitialData();
+    return () => {
+      _isMounted = false;
+    }
+  }, []);
+
+  if(state.loading) return null;
+
   return (
     <NavigationContainer>
       <MainStack />
