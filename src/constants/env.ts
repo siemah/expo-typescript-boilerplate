1087807cf00a74106afbf7b15d57c4fcc20f91ec
siemah <28986331+siemah@users.noreply.github.com>
2021-07-19 09:21:28 +0100

    change keys

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,6 +1,6 @@
 const ENV_VARS = {
   sentry: {
-    dsn: 'https://615ee0863a584c08b7791225fd44c0ea@o617641.ingest.sentry.io/5869725',
+    dsn: 'https://<your-sentry-dsn>.ingest.sentry.io/5869725',
     debug: false
   },
   appScheme: 'appdropzik://'
